d856aca241e48799b42fb85479109ca6e508575e
Yuvraj pandey <hellofirstskout@gmail.com>
2026-05-05 22:12:02 +0530

    Create sitemap.ts
    SEO

---
+++ sitemap.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    { url: 'https://www.firstskout.com/', lastModified: new Date(), changeFrequency: 'weekly', priority: 1.0 },
+    { url: 'https://www.firstskout.com/about-us', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
+    { url: 'https://www.firstskout.com/creators', lastModified: new Date(), changeFrequency: 'weekly', priority: 0.9 },
+    { url: 'https://www.firstskout.com/tools', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
+    { url: 'https://www.firstskout.com/blog', lastModified: new Date(), changeFrequency: 'daily', priority: 0.9 },
+    { url: 'https://www.firstskout.com/appointment', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.7 },
+    { url: 'https://www.firstskout.com/legal', lastModified: new Date(), changeFrequency: 'yearly', priority: 0.3 },
+  ]
+}
